8bbc9932d3d082d9b0d624793e3187218fe276ed
chandhu005 <115386409+chandhu005@users.noreply.github.com>
2023-01-25 19:13:19 +0530

    Add files via upload

---
+++ introduction to Arrays/findDisappearElement.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class findDisappearElement {
+public static List<Integer> disappear(int[] arr,int n)
+{
+	List<Integer> res=new ArrayList<Integer>();
+
+	int i=0;
+	while(i<n)
+	{
+		int correct=arr[i]-1;
+		if(arr[i]!=arr[correct])
+		{
+			swap(arr,i,correct);
+		}
+		else
+		{
+			i++;
+		}
+	}
+	for(int index=0;index<arr.length;index++)
+	{
+		if(arr[index]!=index+1)
+		{  
+			res.add(index+1); 
+		}
+	}
+	return res;
+}
+public static void swap(int[] arr,int first,int second)
+{
+	int temp=arr[first];
+	 arr[first]=arr[second];
+	 arr[second]=temp;
+	
+}
+	public static void main(String[] args) {
+
+	    Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+        	a[i]=sc.nextInt();
+        }
+        List<Integer> output = disappear(a, n);
+      //  for(int i=0;i<n;i++)
+        System.out.println(output);
+
+	}
+
+}
