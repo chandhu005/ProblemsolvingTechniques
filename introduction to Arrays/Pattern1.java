2fb163fa1e695dafe7f99929925fd0ad15021f74
chandhu005 <115386409+chandhu005@users.noreply.github.com>
2023-01-30 14:00:12 +0530

    Add files via upload

---
+++ introduction to Arrays/Pattern1.java
@@ -0,0 +1,17 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<=5;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(j%2);
+			}	System.out.println();
+		}
+
+	}
+
+}
