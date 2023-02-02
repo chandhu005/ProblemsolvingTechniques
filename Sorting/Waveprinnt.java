40c13b5a426600a2d4d491d8d9f86bd8ff2b1493
chandhu005 <115386409+chandhu005@users.noreply.github.com>
2023-02-02 20:17:26 +0530

    Update Waveprinnt.java

--- Sorting/Waveprinnt.java
+++ Sorting/Waveprinnt.java
@@ -20,6 +20,8 @@ public class Waveprinnt {
 		}
 
 	}
+	//a1 >= a2 <= a3 >= a4 <= a5.....
+	
 	 public static int[] wave(int[] A) {
 	        Arrays.sort(A);
 	        for(int i=0;i<A.length-1;i=i+2){
