e3609344fbc7115f82032f59b2aeee3cb39ad793
chandhu005 <115386409+chandhu005@users.noreply.github.com>
2023-02-22 08:54:03 +0530

    Create CheckREctangleOverLappingornot.java

---
+++ MATHSINCODING/CheckREctangleOverLappingornot.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int solve(int A, int B, int C, int D, int E, int F, int G, int H) {
+        
+        if((C<=E) || (D<=F) ||(A>=G)||(B>=H)){
+            return 0;
+        }
+        return 1;
+    }
+}
