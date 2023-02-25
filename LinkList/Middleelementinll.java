4644ec93967dbe40e5a72c2dfca607e3550a76e2
chandhu005 <115386409+chandhu005@users.noreply.github.com>
2023-02-25 19:09:48 +0530

    Create Middleelementinll.java

---
+++ LinkList/Middleelementinll.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public int solve(ListNode A) {
+        ListNode slow=A;
+        ListNode fast=A;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+         if(fast.next == null){
+          return slow.val;
+      }
+        return slow.next.val;
+    }
+}
